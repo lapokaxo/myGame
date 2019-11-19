56da6cef6bd2da1c05e0dafa764c2ce4789d6937
lapokaxo <57005567+lapokaxo@users.noreply.github.com>
2019-11-19 15:53:09 +0100

    samolo t na koncu

--- js/endgame-scene.js
+++ js/endgame-scene.js
@@ -6,10 +6,15 @@ export default class EndGameScene extends Phaser.Scene {
   }
   preload() {
     this.load.image("endgame", "assets/youdead.png");
+    this.load.spritesheet("ship", "assets/ship.png", {
+      frameWidth: 32,
+      frameHeigh: 32
+    });
   }
   create() {
     this.background = this.add.image(256 / 2, 272 / 2, "endgame");
     this.rKeys = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    this.ship = this.physics.add.sprite(256 / 2, 370 / 2, "ship");
   }
   update() {
     if (this.rKeys.isDown) {
--- js/start-scene.js
+++ js/start-scene.js
@@ -6,10 +6,15 @@ export default class StartScene extends Phaser.Scene {
   }
   preload() {
     this.load.image("play", "assets/start.png");
+    this.load.spritesheet("ship", "assets/ship.png", {
+      frameWidth: 32,
+      frameHeigh: 32
+    });
   }
   create() {
     this.background = this.add.image(256 / 2, 272 / 2, "play");
     this.rKeys = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    this.ship = this.physics.add.sprite(256 / 2, 272 / 2, "ship");
   }
   update() {
     if (this.rKeys.isDown) {
